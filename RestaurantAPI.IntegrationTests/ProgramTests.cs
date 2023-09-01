f1fd76e02a22fd55d17c166dc3f9e5d440517555
Adrian Domin <adriandominx@gmail.com>
2023-09-01 09:32:50 +0200

    Add ProgramTests

---
+++ RestaurantAPI.IntegrationTests/ProgramTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Testing;
+using RestaurantAPI.Controllers;
+
+namespace RestaurantAPI.IntegrationTests
+{
+    public class ProgramTests : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly WebApplicationFactory<Program> _factory;
+
+        public ProgramTests(WebApplicationFactory<Program> factory)
+        {
+            var controllerTypes = typeof(Program)
+                .Assembly
+                .GetTypes()
+                .Where(t => t.IsSubclassOf(typeof(ControllerBase)))
+                .ToList();
+
+            _factory = factory.WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureServices(services =>
+                {
+                    controllerTypes.ForEach(c => services.AddScoped(c));
+                });
+            });
+        }
+    }
+}
